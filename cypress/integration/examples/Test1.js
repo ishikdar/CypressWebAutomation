0129b40f54638f26aee8a68d9c902f4af3f10d84
ishikdar <shikder.imran@yahoo.com>
2024-06-01 15:43:03 +0600

    1st test case

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -1,1 +1,14 @@
-test
+describe('My first test suit', function()
+
+{
+it('My first test case', function()
+{
+    cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
+})
+
+it('My second test case', function()
+{
+    //test steps
+})
+
+})
