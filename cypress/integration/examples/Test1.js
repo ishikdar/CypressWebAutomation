c8b2fd7a52b6949aee0bad5c0b5eb3c547029bff
ishikdar <shikder.imran@yahoo.com>
2024-06-06 12:58:44 +0600

    Class 18

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -1,14 +1,23 @@
+/// <reference types="cypress"/>
+
 describe('My first test suit', function()
 
 {
 it('My first test case', function()
 {
-    cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
+    cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/")
+    //class 16 (Css selectors)
+    cy.get('.search-keyword').type('ca')
+
+    //class 17 (assertion)
+    cy.wait(2000)
+    //cy.get('.product').should('have.length',4)
+
+    //class 18 how to handle invisible classes
+    cy.get('.product:visible').should('have.length',4)
+
 })
 
-it('My second test case', function()
-{
-    //test steps
-})
+
 
 })
