768f7dfafa051777ef07ba0de74c9d84e40b859f
ishikdar <shikder.imran@yahoo.com>
2024-06-08 12:58:48 +0600

    class 20

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -16,6 +16,11 @@ it('My first test case', function()
     //class 18 how to handle invisible classes
     cy.get('.product:visible').should('have.length',4)
 
+    //class 20: parent child chaining
+    cy.get('.products').find('.product').should('have.length',4)
+
+    //another method of parent child chaining 
+    cy.get('.products').find('.product').eq(2).contains('ADD TO CART').click()
 })
 
 
